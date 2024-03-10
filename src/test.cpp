1d70371122ac773a506b08fe0737341c512b9e4b
“ <56799376+Morph1c@users.noreply.github.com>
2024-03-10 16:20:33 +0000

    Testing the parameters

--- src/test.cpp
+++ src/test.cpp
@@ -3,11 +3,14 @@
 //
 
 #include <iostream>
+#include "optimizer.hpp"
 
-using std::cout
+using std::cout;
 
 int main(void){
+    params my_param;
     cout << "First version of test" << std::endl;
+    cout << "Default number of max iterations is " << my_param.k_max << std::endl;
 
     return 0;
 }
